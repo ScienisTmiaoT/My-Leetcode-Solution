2fe719f1ad7b364dd5356a607b5ef873c01bd907
ScienisTmiaoT <1730402302@qq.com>
2016-12-26 21:25:55 +0800

    improve effciency
    use priority queue

--- 373-Find-K-Pairs-with-Smallest-Sums/k_pairs.h
+++ 373-Find-K-Pairs-with-Smallest-Sums/k_pairs.h
@@ -8,6 +8,8 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <queue>
+#include <iostream>
 
 class KPair {
 public:
@@ -36,6 +38,42 @@ public:
         size_t len = len1 * len2;
         for(int i = 0; (i < k) && (i < len); i++){
             min_pair.push_back(pair_sum[i].first);
+        }
+        return min_pair;
+    }
+
+    // use priority queue, every time calculate sub matrix, then add
+    // next two pairs into queue, for example, assume we have v1 = {1, 2, 3},
+    // v2 = {4, 5, 6}, then 4-smallest must come from sub vector {1, 2} and
+    // {4, 5}
+    std::vector<std::pair<int, int>> kSmallestPairsTwo(std::vector<int>& nums1,
+                                                    std::vector<int>& nums2, int k) {
+        std::vector<std::pair<std::pair<int, int>, int>> pair_sum;
+        std::vector<std::pair<int, int>> min_pair;
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+        // judge empty set
+        if(len1 == 0 || len2 == 0)
+            return min_pair;
+        auto cmp = [&nums1, &nums2](std::pair<int, int> a, std::pair<int, int>
+        b){
+            return nums1[a.first] + nums2[a.second] > nums1[b.first] +
+                                                              nums2[b.second];
+        };
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int,
+                int>>, decltype(cmp)> prio_que(cmp);
+        prio_que.emplace(0, 0);
+        int s = std::min(k, len1 * len2);
+        while(s-- > 0){
+            int first = prio_que.top().first;
+            int second = prio_que.top().second;
+            std::pair<int, int> p(nums1[first], nums2[second]);
+            min_pair.push_back(p);
+            prio_que.pop();
+            if(first + 1 < len1)
+                prio_que.emplace(first + 1, second);
+            if((first == 0) && (second + 1 < len2))
+                prio_que.emplace(first, second + 1);
         }
         return min_pair;
     }
--- 373-Find-K-Pairs-with-Smallest-Sums/main.cpp
+++ 373-Find-K-Pairs-with-Smallest-Sums/main.cpp
@@ -4,13 +4,12 @@
 
 #include "k_pairs.h"
 
-#include <iostream>
-
 int main() {
     std::vector<int> nums1{1, 1, 2};
     std::vector<int> nums2{1, 2, 3};
     KPair *kp = new KPair();
-    std::vector<std::pair<int, int>> res = kp->kSmallestPairs(nums1, nums2, 10);
+    std::vector<std::pair<int, int>> res = kp->kSmallestPairsTwo(nums1, nums2,
+                                                               10);
     for(auto i : res)
         std::cout << i.first << " " << i.second << std::endl;
     return 0;
